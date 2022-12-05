f0f93f3e95d54ca30fdc12e392983698c88b1b24
Zeeshan Ahmad <zeeshan.za.ahmad@gmail.com>
2022-12-05 19:47:53 +0500

    Un comment key combinitions

--- src/Components/RichTextWithDropDown/richTextAccessibility.js
+++ src/Components/RichTextWithDropDown/richTextAccessibility.js
@@ -3,20 +3,18 @@ const { hasCommandModifier,usesMacOSHeuristics } = KeyBindingUtil;
 export const OPEN_DROPDOWN = "OPEN-DROPDOWN";
 
 export const handleRichTextKeyDown = (event, closeDropDown) => {
-  console.log(event.keyCode, event.ctrlKey,hasCommandModifier(event), usesMacOSHeuristics(event))
   if (event.keyCode === 75 /* `K` key */ && hasCommandModifier(event)) {
     return OPEN_DROPDOWN;
   };
   
   // Ctrl + Space
   if (event.keyCode === 32 && event.ctrlKey && (hasCommandModifier(event) || usesMacOSHeuristics(event))) {
-    console.log("sin")
     return OPEN_DROPDOWN;
   }
   
-  // if ([27, 37, 39, 32, 17].indexOf(event.keyCode) !== -1) {
-  //   closeDropDown();
-  // }
+  if ([27, 37, 39, 32, 17].indexOf(event.keyCode) !== -1) {
+    closeDropDown();
+  }
   
   return getDefaultKeyBinding(event);
 };
